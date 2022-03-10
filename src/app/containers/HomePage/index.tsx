aa9ab26bf2fa2fe8cc72f94a89cccea6f41a3152
JosePedro <guedes.jose@gmail.com>
2022-03-10 16:50:33 +0000

    TopSide LandingPage Finished

--- src/app/containers/HomePage/index.tsx
+++ src/app/containers/HomePage/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
 import NavBar from "../../components/NavBar";
+import TopSection from "./TopSection";
 
 const PageContainer = styled.div`
   ${tw`
@@ -17,6 +18,7 @@ export function HomePage() {
   return (
     <PageContainer>
       <NavBar />
+      <TopSection />
     </PageContainer>
   );
 }
